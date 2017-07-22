807885875549b3cdfcb31c5669fd4d6896eed35e
Francis <FrancisPBaker@gmail.com>
2017-07-22 18:58:27 -0400

    Push
    Added more of my JS Class

--- tutorials/_js/site.js
+++ tutorials/_js/site.js
@@ -3,23 +3,21 @@ $(document).ready(function() {
 	$('a[href^="http://"]').attr('target','_blank');
 	
 	//position resources panel on screen
-	$('#resources').show().delay(500).animate({ left : '-25em' }, 250).animate({left:'-29em'},250);
+	$('#resources').show().delay(500).animate({ left : '-48em' }, 250).animate({left:'-51em'},250);
 	
 	// resources panel
-	var panel=false;
-	$('.open').click(function() {
-		if (!panel) {
+	$('.open').toggle(
+		function() {
 			$(this).text('-');
 			$('#resources').animate({
 				left : 0
 			}, 250); // end animate
-			panel=true;
-		} else {
+		},
+		function() {
 			$(this).text('+');
 			$('#resources').animate({
-				left : '-29em'
+				left : '-51em'
 			}, 250); // end animate
-			panel=false;
 		}
-	});
+	);
 }); // end ready
